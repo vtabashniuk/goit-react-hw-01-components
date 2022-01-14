275df0a4836370c219bc6927fab048707c7d6bab
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2022-01-14 06:03:11 +0200

    Добавляет стилизацию для компонента задания - 1

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
-import defaultImage from "../defaultImage.png";
+import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from '../defaultImage.png';
+import styles from './Profile.module.css';
 
-const defaultTitle1 = "";
+const defaultTitle1 = '';
 const defaultFollowers = 0;
 const defaultViews = 0;
 const defaultLikes = 0;
@@ -17,27 +18,27 @@ const Profile = ({
   views,
   likes,
 }) => (
-  <div class="profile">
-    <h2>{title}</h2>
-    <div class="description">
-      <img src={avatar} alt="User avatar" class="avatar" />
-      <p class="name">{userName}</p>
-      <p class="tag">@{tag}</p>
-      <p class="location">{location}</p>
+  <div className={styles.profile}>
+    <h2 className={styles.title}>{title}</h2>
+    <div className={styles.description}>
+      <img src={avatar} alt="User avatar" className={styles.avatar} />
+      <p className={styles.name}>{userName}</p>
+      <p className={styles.tag}>@{tag}</p>
+      <p className={styles.location}>{location}</p>
     </div>
 
-    <ul class="stats">
-      <li>
-        <span class="label">Followers</span>
-        <span class="quantity">{followers}</span>
+    <ul className={styles.stats}>
+      <li className={styles.stats_item}>
+        <span className={styles.label}>Followers</span>
+        <span className={styles.quantity}>{followers}</span>
       </li>
-      <li>
-        <span class="label">Views</span>
-        <span class="quantity">{views}</span>
+      <li className={styles.stats_item}>
+        <span className={styles.label}>Views</span>
+        <span className={styles.quantity}>{views}</span>
       </li>
-      <li>
-        <span class="label">Likes</span>
-        <span class="quantity">{likes}</span>
+      <li className={styles.stats_item}>
+        <span className={styles.label}>Likes</span>
+        <span className={styles.quantity}>{likes}</span>
       </li>
     </ul>
   </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { root } from "./refs";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { root } from './refs';
+import App from './components/App';
+import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  root
+  root,
 );
-
